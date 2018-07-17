da9f2e3753a5b28563dc204207a14979e173bb69
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-17 12:51:14 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -5,4 +5,9 @@ class Character < ActiveRecord::Base
   def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
   end
+
+  def build_show(:name)
+    new_show = Show.create(:name)
+    self.shows << new_show
+  end
 end
